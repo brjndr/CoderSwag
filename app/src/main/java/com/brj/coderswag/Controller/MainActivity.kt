67a75faab64f596c00e1eb40d8257911d7161557
Brajendra <brajendrarai1@gmail.com>
2020-07-22 10:37:32 +0530

    list view with adapter

--- app/src/main/java/com/brj/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/brj/coderswag/Controller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.brj.coderswag.Controller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.brj.coderswag.Adapters.CategoryAdapter
 import com.brj.coderswag.Model.Category
 import com.brj.coderswag.R
@@ -12,15 +13,17 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-//    lateinit var adapter: ArrayAdapter<Category>
         lateinit var adapter: CategoryAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         adapter = CategoryAdapter(this, DataService.categories)
-
         categoryListView.adapter = adapter
 
+//        categoryListView.setOnItemClickListener { adapterView, view, i, l ->  // for listView
+//            val category = DataService.categories[i]
+//            Toast.makeText(this, "You clicked on ${category.title} cell.", Toast.LENGTH_SHORT).show()
+//        }
     }
 }
